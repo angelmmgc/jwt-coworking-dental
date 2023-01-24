1cd8d9e0bce3937bc435864741fa5d46342802bf
angel martin <angelmmgc@gmail.com>
2023-01-24 21:00:42 +0100

    se añade metodo deleteByUserName con una native query

--- src/main/java/com/example/jwtcoworkingdental/security/repository/UsuarioRepository.java
+++ src/main/java/com/example/jwtcoworkingdental/security/repository/UsuarioRepository.java
@@ -3,8 +3,13 @@ package com.example.jwtcoworkingdental.security.repository;
 
 import com.example.jwtcoworkingdental.security.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.TransactionScoped;
+import javax.transaction.Transactional;
 import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
@@ -14,6 +19,15 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findUsuarioByEmail(String email);
     boolean existsByUserName(String username);
     boolean existsByEmail(String email);
+
+    /**
+     * query para borrar un usuario segun id, solo puede ser admin
+     * @param user_id
+     */
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM usuarios WHERE id = :user_id", nativeQuery = true)
+    void deleteByUserName(@Param("user_id") Long user_id);
 
 
 }
