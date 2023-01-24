87878931f74fa12a0046cbe7b35189491b8e6c15
angel martin <angelmmgc@gmail.com>
2023-01-24 20:56:47 +0100

    refactoriza metodo updatemap

--- src/main/java/com/example/jwtcoworkingdental/security/mapper/NuevoUsuarioDTOtoUsuario.java
+++ src/main/java/com/example/jwtcoworkingdental/security/mapper/NuevoUsuarioDTOtoUsuario.java
@@ -4,8 +4,11 @@ package com.example.jwtcoworkingdental.security.mapper;
 
 import com.example.jwtcoworkingdental.mapper.IMapper;
 import com.example.jwtcoworkingdental.security.dto.NuevoUsuario;
+import com.example.jwtcoworkingdental.security.entity.Rol;
 import com.example.jwtcoworkingdental.security.entity.Usuario;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 public class NuevoUsuarioDTOtoUsuario implements IMapper<NuevoUsuario, Usuario> {
@@ -20,15 +23,13 @@ public class NuevoUsuarioDTOtoUsuario implements IMapper<NuevoUsuario, Usuario> 
         usuario.setEmail(in.getEmail());
         usuario.setTelefono(in.getTelefono());
         usuario.setPassword(in.getPassword());
-
         return usuario;
     }
 
 
-    public Usuario updatemap(NuevoUsuario in, Long id) {
+    public Usuario updatemap(NuevoUsuario in, Long id,Set<Rol> rol) {
 
         Usuario usuario = new Usuario();
-
         usuario.setId(id);
         usuario.setUserName(in.getUserName());
         usuario.setNombre(in.getNombre());
@@ -36,7 +37,7 @@ public class NuevoUsuarioDTOtoUsuario implements IMapper<NuevoUsuario, Usuario> 
         usuario.setEmail(in.getEmail());
         usuario.setTelefono(in.getTelefono());
         usuario.setPassword(in.getPassword());
-
+        usuario.setRoles(rol);
         return usuario;
     }
 
