513544130a772e14ec6b5f0020a2ea0cbaf9f377
angel martin <angelmmgc@gmail.com>
2023-01-26 20:02:33 +0100

    se añade funcion findByNombre a la interfaz

--- src/main/java/com/example/jwtcoworkingdental/repositories/generico/GenericoRepository.java
+++ src/main/java/com/example/jwtcoworkingdental/repositories/generico/GenericoRepository.java
@@ -1,11 +1,15 @@
 package com.example.jwtcoworkingdental.repositories.generico;
 
 import com.example.jwtcoworkingdental.entities.generico.Generico;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface GenericoRepository<E extends Generico, ID extends Serializable> extends JpaRepository<E, ID> {
+
+    Optional<E> findByNombre(String nombre);
 }
