9cc2e286590ca566e6d9456fa5c5ba2dd46f611c
angel martin <angelmmgc@gmail.com>
2023-01-26 20:00:57 +0100

    se añade función finByNombre

--- src/main/java/com/example/jwtcoworkingdental/repositories/clinica/ClinicaRepository.java
+++ src/main/java/com/example/jwtcoworkingdental/repositories/clinica/ClinicaRepository.java
@@ -3,5 +3,14 @@ package com.example.jwtcoworkingdental.repositories.clinica;
 import com.example.jwtcoworkingdental.entities.clinica.Clinica;
 import com.example.jwtcoworkingdental.repositories.generico.GenericoRepository;
 
+import java.util.Optional;
+
 public interface ClinicaRepository extends GenericoRepository<Clinica,Long> {
+    @Override
+    Optional<Clinica> findByNombre(String nombre);
+
+
+
+
+
 }
